db150bef87ac796204e9d059f2c3e127da490932
Sydney <sydney.kiersten@gmail.com>
2021-04-22 11:16:53 -0400

    Initial commit

---
+++ src/main/java/com/sydneyhass/weatherstation/WeatherStationApplication.java
@@ -0,0 +1,13 @@
+package com.sydneyhass.weatherstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherStationApplication.class, args);
+	}
+
+}
