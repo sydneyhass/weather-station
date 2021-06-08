c4f1efd0d78b33b5f1d757e3dd3a573bbfffe45b
Sydney <sydney.kiersten@gmail.com>
2021-06-07 22:05:32 -0400

    Added Detail page and updated detail compoent

---
+++ src/frontend/src/components/Details.js
@@ -0,0 +1,10 @@
+import { Container } from "@chakra-ui/layout";
+import React from "react";
+
+export default function Details() {
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
+}
