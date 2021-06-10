9ae27bd6fb57f7a7e89f856c1dc460bbdad604bb
Sydney <sydney.kiersten@gmail.com>
2021-06-09 23:17:45 -0400

    Started error handling for 404

---
+++ src/frontend/src/pages/FourOhFourPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Error from "../components/Error";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function FourOhFourPage() {
+  return (
+    <>
+      <Header />
+      <Error />
+      <Footer />
+    </>
+  );
+}
